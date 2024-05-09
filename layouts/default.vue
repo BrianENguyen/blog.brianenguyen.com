928bbce2946aecb3baceb324daf427e3f4c9bc3f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-09 15:45:48 -0500

    style: back to the 3 column layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="grid grid-cols-9">
+  <div class="grid grid-cols-12 gap-0 m-0 p-0">
+    <div class="col-span-2 p-1"></div>
     <div
-      class="col-span-7 m-0 mx-auto p-12 min-h-100vh bg-white dark:bg-#1d1d1d border-l-solid border-r-solid border-gray border-1"
+      class="col-span-8 m-0 mx-auto p-12 min-h-100vh bg-white dark:bg-#1d1d1d border-l-solid border-r-solid border-gray border-1"
     >
       <slot />
     </div>
-    <div class="col-span-2 p-2 w-full">
+    <div class="col-span-2 p-1">
       <span class="text-4xl">Brian Nguyen</span>
       <Navbar />
     </div>
