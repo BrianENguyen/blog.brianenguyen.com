3c5bf9b92dd74234d939de3438b771324055de97
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-09 14:25:57 -0500

    style: grid layout

--- content/index.md
+++ content/index.md
@@ -1,3 +1,6 @@
 # Brian Nguyen
 
-<p class='text-center font-italic'><em>An opinionated Nuxt starter template</em></p>
+Hi! I'm a software engineer. This website is mainly designed for me to document
+my life and to explore interests other than software development. I guess this is mainly
+a blog website, but I'm still thinking of what other features to put on here.
+Glad you decided to check out this site!
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,13 @@
 <template>
-  <div
-    class="m-0 mx-auto max-w-900px p-4 h-100vh bg-white dark:bg-#1d1d1d border-l-solid border-r-solid border-gray border-1"
-  >
-    <slot />
+  <div class="grid grid-cols-12">
+    <div class="col-span-2 p-6">
+      <span class="text-4xl">Brian Nguyen</span>
+    </div>
+    <div
+      class="col-span-8 m-0 mx-auto max-w-900px p-4 min-h-100vh bg-white dark:bg-#1d1d1d border-l-solid border-r-solid border-gray border-1"
+    >
+      <slot />
+    </div>
+    <div class="col-span-2"></div>
   </div>
 </template>
