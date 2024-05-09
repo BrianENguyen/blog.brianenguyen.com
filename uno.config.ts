b6f3c2f4c8938d0ba16c0a4d03876c9e5e44d7c1
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-09 15:46:04 -0500

    config: uno colors

--- uno.config.ts
+++ uno.config.ts
@@ -9,6 +9,13 @@ export default defineConfig({
       md: '992px',
       lg: '1200px',
     },
+    colors: {
+      brianBlue: '#79c2d0',
+      brianBlueDark: '#0495b1',
+      brianBlueLight: '#d4f3f9',
+      grayLight: '#282c34',
+      black: '#202020',
+    },
   },
   presets: [presetIcons({ prefix: 'i-' }), presetUno()],
 });
