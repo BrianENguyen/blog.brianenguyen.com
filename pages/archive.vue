361bb6cb05a33ac8d1aa0f89353cf100bbb92b91
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-10 13:42:43 -0500

    fix: routing from archive

--- pages/archive.vue
+++ pages/archive.vue
@@ -9,12 +9,20 @@ function getYear(dateString) {
   }
   return "";
 }
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.toLocaleString('en-US', { month: 'short' }).toUpperCase();
+  const day = date.getDate().toString().padStart(2, '0');
+
+  return `${year} ${month} ${day}`;
+}
 
 const blogs = await queryContent('blog')
   .only(['title', 'date', 'description', '_path'])
   .sort({ date: -1 })
   .find()
-console.log(blogs)
+
 let blogsByYear = {};
 
 blogs.forEach(blog => {
@@ -30,10 +38,15 @@ blogs.forEach(blog => {
 </script>
 
 <template>
-  <div v-for="(blogs, i) in blogsByYear" :key="i">
-    <h2>{{ i }}</h2>
-    <div v-for="blog in blogs">
-      {{blog.title}}
+  <div>
+    <h1>Archive</h1>
+    <div v-for="(blogs, i) in blogsByYear" :key="i">
+      <h2>{{ i }}</h2>
+      <div v-for="blog in blogs" :key="blog.title" class="my-2">
+        <p>{{ formatDate(blog.date) }}</p>
+        <NuxtLink :to="blog._path">{{ blog.title }}</NuxtLink>
+        <p>{{ blog.description }}</p>
+      </div>
     </div>
   </div>
 </template>
