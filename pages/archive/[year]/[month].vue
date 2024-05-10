0b39eabbf22abf6649c8245e370eacf643d47607
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-10 16:02:20 -0500

    feaet: month view

--- pages/archive/[year]/[month].vue
+++ pages/archive/[year]/[month].vue
@@ -1,9 +1,69 @@
 <script setup>
-const route = useRoute()
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.toLocaleString('en-US', { month: 'short' }).toUpperCase();
+  const day = date.getDate().toString().padStart(2, '0');
 
-console.log(route.params);
+  return `${year} ${month} ${day}`;
+}
+
+function convertMonthToName(month) {
+  const date = new Date(2024, month - 1, 1);
+  return date.toLocaleDateString('default', { month: 'long' });
+}
+
+function getYear(dateString) {
+  let year = "";
+  const regex = /\d{4}/;
+  const match = dateString.match(regex);
+  if (match) {
+    year = match[0];
+    return year;
+  }
+  return "";
+}
+
+function getMonth(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleString('en-US', { month: 'long' });
+}
+
+function convertMonthToDigit(month) {
+  const date = Date.parse(month + "1, 1999");
+  return new Date(date).getMonth() + 1
+}
+
+const route = useRoute()
+const yearParam = route.params.year;
+const monthParam = route.params.month;
+
+const blogs = await queryContent('blog')
+  .only(['title', 'date', '_path'])
+  .sort({ date: -1 })
+  .find();
+
+const filteredBlogs = [];
+blogs.forEach(blog => {
+  let year = getYear(blog.date);
+  let month = convertMonthToDigit(getMonth(blog.date));
+  if (yearParam === year && month == monthParam) {
+    filteredBlogs.push(blog);
+  }
+})
 </script>
 
 <template>
-  <h1>You've successfully reached the month view</h1>
+  <section :id="`${yearParam}-${monthParam}`">
+    <h1>Blogs from {{ yearParam }} {{ convertMonthToName(monthParam) }}</h1>
+    <div v-for="(blog, i) in filteredBlogs" :key="i">
+      <NuxtLink
+        :to="blog._path"
+        class="text-black dark:!text-white no-underline hover:!text-brian-blue"
+      >
+        <time class="block text-gray">{{ formatDate(blog.date) }}</time>
+        <span>{{ blog.title }}</span>
+      </NuxtLink>
+    </div>
+  </section>
 </template>
