a284fae220d7da4b4701b2f27f7a4a48d52e9bc9
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-10 16:06:29 -0500

    feat: year view

--- pages/archive/[year]/index.vue
+++ pages/archive/[year]/index.vue
@@ -1,7 +1,52 @@
 <script setup>
+function getYear(dateString) {
+  let year = "";
+  const regex = /\d{4}/;
+  const match = dateString.match(regex);
+  if (match) {
+    year = match[0];
+    return year;
+  }
+  return "";
+}
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.toLocaleString('en-US', { month: 'short' }).toUpperCase();
+  const day = date.getDate().toString().padStart(2, '0');
+
+  return `${year} ${month} ${day}`;
+}
+
 const route = useRoute()
-const year = route.params.year;
+const yearParam = route.params.year;
+
+const blogs = await queryContent('blog')
+  .only(['title', 'date', '_path'])
+  .sort({ date: -1 })
+  .find();
+
+const filteredBlogs = [];
+
+blogs.forEach(blog => {
+  let year = getYear(blog.date);
+  if (yearParam === year) {
+    filteredBlogs.push(blog);
+  }
+});
 </script>
 <template>
-  <h1>Blogs in {{ year }}</h1>
+  <section :id="yearParam">
+    <h1>Blogs from {{ yearParam }}</h1>
+    <div v-for="(blog, i) in filteredBlogs" :key="i">
+      <NuxtLink
+        :to="blog._path"
+        class="text-black dark:!text-white no-underline hover:!text-brian-blue"
+      >
+        <time class="block text-gray">{{ formatDate(blog.date) }}</time>
+        <span>{{ blog.title }}</span>
+      </NuxtLink>
+    </div>
+  </section>
 </template>
