f8c3cc612c2727ce24c480e59b0e180085d7e0c6
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-10 14:03:46 -0500

    feat: reworked algorithm to display number of blogs

--- pages/archive.vue
+++ pages/archive.vue
@@ -4,18 +4,15 @@ function getYear(dateString) {
   const regex = /\d{4}/;
   const match = dateString.match(regex);
   if (match) {
-    year = match[0]
+    year = match[0];
     return year;
   }
   return "";
 }
-function formatDate(dateString) {
-  const date = new Date(dateString);
-  const year = date.getFullYear();
-  const month = date.toLocaleString('en-US', { month: 'short' }).toUpperCase();
-  const day = date.getDate().toString().padStart(2, '0');
 
-  return `${year} ${month} ${day}`;
+function getMonth(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleString('en-US', { month: 'long' });
 }
 
 const blogs = await queryContent('blog')
@@ -26,26 +23,31 @@ const blogs = await queryContent('blog')
 let blogsByYear = {};
 
 blogs.forEach(blog => {
-  let year = getYear(blog.date);
-  year = `0_${year}`;
-  if (blogsByYear[year] === undefined) {
-    blogsByYear[year] = [];
-    blogsByYear[year].push(blog)
+  let year = `0_${getYear(blog.date)}`;
+  let month = getMonth(blog.date);
+
+  if (!blogsByYear[year]) {
+    blogsByYear[year] = {};
   }
-  else {
-    blogsByYear[year].push(blog)
+
+  if (!blogsByYear[year][month]) {
+    blogsByYear[year][month] = [];
   }
-})
+
+  blogsByYear[year][month].push(blog);
+});
 </script>
 
 <template>
   <div>
     <h1>Archive</h1>
-    <div v-for="(blogs, i) in blogsByYear" :key="i">
-      <h2>{{ i.replace(/^\d+_/, '') }}</h2>
-      <div v-for="blog in blogs" :key="blog.title" class="my-2">
-        <p>{{ formatDate(blog.date) }}</p>
-        <NuxtLink :to="blog._path">{{ blog.title }}</NuxtLink>
+    <div v-for="(yearBlogs, year) in blogsByYear" :key="year">
+      <h2>{{ year.replace(/^\d+_/, '' ) }}</h2>
+      <div v-for="(monthBlogs, month) in yearBlogs" :key="month">
+        <h3>
+          {{ month }} ({{ monthBlogs.length }}
+          {{ monthBlogs.length > 1 ? 'blogs' : 'blog' }})
+        </h3>
       </div>
     </div>
   </div>
