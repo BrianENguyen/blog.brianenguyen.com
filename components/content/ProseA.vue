712b2c505b316f2426925d1746d9413bb8a34a4e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-10 21:46:00 -0500

    feat: prose components

---
+++ components/content/ProseA.vue
@@ -0,0 +1,14 @@
+<template>
+  <a :href="href" target="_blank">
+    <slot />
+  </a>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  href: {
+    type: String,
+    default: ''
+  },
+})
+</script>
