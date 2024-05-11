8c5b99ba4d4d5c46153f793cf600b7dfef219457
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-11 14:11:46 -0500

    feat: limit number of blogs

--- pages/blog.vue
+++ pages/blog.vue
@@ -26,12 +26,13 @@ useHead({
       Want to see the full list? Check out the archives!
     </p>
     <ContentList :query="query" v-slot="{ list }">
-      <div v-for="(blog, blogIndex) in list" :key="blog._path">
+      <div v-for="(blog, i) in list.slice(0,10)" :key="i">
         <div
           class="rounded-md 
             dark:bg-[#131313]
               min-h-[200px] h-auto
               p-4
+              my-4
               transition-colors
               border-1px dark:border-gray-light border-solid
               border-l-[4px] hover:border-l-brian-blue hover:border-[#434a57]"
@@ -41,7 +42,6 @@ useHead({
           <p>{{ blog.description }}</p>
           <NuxtLink :to="blog._path">Read More</NuxtLink>
         </div>
-        <hr class="text-[#454545]" v-if="!(blogIndex === list.length - 1)" />
       </div>
     </ContentList>
   </section>
