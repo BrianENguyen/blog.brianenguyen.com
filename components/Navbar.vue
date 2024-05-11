481993e56b5e105708fe93ebd7d112ffb0036747
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-10 21:26:52 -0500

    style: fix navbar bg

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -38,14 +38,22 @@ function toggleMobileMenu() {
     </ul>
   </nav>
   <nav
-    class="fixed sm:hidden bg-#0e1111 fixed w-[100%] h-100px z-[1] top-0 transition duration-300"
+    class="fixed sm:hidden bg-white dark:bg-#0e1111 fixed w-[100%] h-100px z-[1] top-0 left-0 transition duration-300 shadow-2xl"
   >
     <div
-      class="py-8 bg-#0e1111 max-w-screen-xl md:flex items-center justify-between mx-auto filter-none"
+      class="i-solar-sun-outline dark:i-solar-moon-line-duotone 
+                cursor-pointer text-3xl text-[#0495b1] hover:text-[#015768]
+                transition-color duration-300 dark:text-[#79c2d0] dark:hover:text-[#efefef]"
+      @click="toggleDark()"
+    />
+    <div
+      class="py-8 bg-white dark:bg-#0e1111 md:flex items-center justify-between mx-auto filter-none"
     >
       <div class="absolute  md:static top-8 md:flex items-center">
         <NuxtLink to="/" class="decoration-none ml-12">
-          <span class="text-white text-2xl font-bold"> Brian's Blog </span>
+          <span class="text-black dark:text-white text-2xl font-bold">
+            Brian's Blog
+          </span>
         </NuxtLink>
       </div>
       <!-- Mobile menu button -->
@@ -54,20 +62,26 @@ function toggleMobileMenu() {
         class="block absolute right-3 md:hidden mr-10px cursor-pointer"
         @click="toggleMobileMenu(isToggled)"
       >
-        <div class="border-1 border-white border-solid w-30px mb-5px" />
-        <div class="border-1 border-white border-solid w-30px mb-5px" />
-        <div class="border-1 border-white border-solid w-30px mb-5px" />
+        <div
+          class="border-1 border-black dark:border-white border-solid w-30px mb-5px"
+        />
+        <div
+          class="border-1 border-black dark:border-white border-solid w-30px mb-5px"
+        />
+        <div
+          class="border-1 border-black dark:border-white border-solid w-30px mb-5px"
+        />
       </div>
       <!-- Mobile Nav list -->
       <ul
         id="mobile-nav-list"
-        class="bg-#0e1111 md:hidden list-none bg-gray-800 w-full p-0 absolute left-0 top-20 shadow-2xl"
+        class="bg-white dark:bg-#0e1111 md:hidden list-none bg-gray-800 w-full p-0 absolute left-0 top-20 shadow-2xl"
         :class="isToggled ? 'block' : 'hidden'"
       >
         <li v-for="link in links" :key="link" @click="isToggled = false">
           <NuxtLink
             :to="link.path"
-            class="text-center py-4 block font-bold text-white decoration-none"
+            class="text-center py-4 block font-bold text-black dark:text-white decoration-none"
           >
             <span class="uppercase zero:display-none md:block">
               {{ link.name }}
