1149981abbcc57ba391128a64517eb72f61bb36d
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-10 22:36:12 -0500

    fix: layout children

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,34 @@
 <template>
-  <!-- Large -->
-  <div class="hidden md:grid grid-cols-12 mt-5">
-    <div class="col-span-2 p-1"></div>
-    <div
-      id="main-content"
-      class="rounded col-span-8 m-0 p-12 min-h-100vh 
+  <div>
+    <!-- Large -->
+    <div class="hidden md:grid grid-cols-12 mt-5">
+      <div class="col-span-2 p-1"></div>
+      <div
+        id="main-content"
+        class="rounded col-span-8 m-0 p-12 min-h-100vh 
             bg-white 
             dark:bg-black "
-    >
-      <slot />
+      >
+        <slot />
+      </div>
+      <div class="col-span-2 p-3">
+        <Navbar />
+      </div>
     </div>
-    <div class="col-span-2 p-3">
-      <Navbar />
-    </div>
-  </div>
 
-  <!-- Medium -->
-  <div class="sm:grid md:hidden grid-cols-12 mt-32 sm:mt-5">
-    <div
-      id="main-content"
-      class="rounded col-span-9 ml-2 p-12 min-h-100vh 
+    <!-- Medium -->
+    <div class="sm:grid md:hidden grid-cols-12 mt-32 sm:mt-5">
+      <div
+        id="main-content"
+        class="rounded col-span-9 ml-2 p-12 min-h-100vh 
             bg-white 
             dark:bg-black "
-    >
-      <slot />
-    </div>
-    <div class="col-span-3 p-3">
-      <Navbar />
+      >
+        <slot />
+      </div>
+      <div class="col-span-3 p-3">
+        <Navbar />
+      </div>
     </div>
   </div>
 </template>
