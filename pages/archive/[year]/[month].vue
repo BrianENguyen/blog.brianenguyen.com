f302178de077fbf6fa731f123e28c3fb98845a6f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-11 20:27:26 -0500

    fix(major): onMount for month view

--- pages/archive/[year]/[month].vue
+++ pages/archive/[year]/[month].vue
@@ -22,27 +22,32 @@ useHead({
 
 const blogs = ref([]);
 
-const data = await queryContent('blog')
-  .only(['title', 'date', '_path'])
-  .sort({ date: -1 })
-  .find();
+async function fetchBlogs() {
+  const data = await queryContent('blog')
+    .only(['title', 'date', '_path'])
+    .sort({ date: -1 })
+    .find();
 
-blogs.value = data;
+  const filteredBlogs = [];
+  data.forEach(blog => {
+    let year = getYear(blog.date);
+    let month = convertMonthToDigit(getMonth(blog.date));
+    if (yearParam === year && month == monthParam) {
+      filteredBlogs.push(blog);
+    }
+  })
+  blogs.value = filteredBlogs;
+}
 
-const filteredBlogs = [];
-blogs.value.forEach(blog => {
-  let year = getYear(blog.date);
-  let month = convertMonthToDigit(getMonth(blog.date));
-  if (yearParam === year && month == monthParam) {
-    filteredBlogs.push(blog);
-  }
+onMounted(() => {
+  fetchBlogs();
 })
 </script>
 
 <template>
-  <section :id="`${yearParam}-${monthParam}`" v-if="filteredBlogs.length">
+  <section :id="`${yearParam}-${monthParam}`" v-if="blogs.length">
     <h1>Blogs from {{ yearParam }} {{ convertMonthToName(monthParam) }}</h1>
-    <div v-for="(blog, i) in filteredBlogs" :key="i" class="mb-4">
+    <div v-for="(blog, i) in blogs" :key="i" class="mb-4">
       <NuxtLink
         :to="blog._path"
         class="text-black dark:!text-white no-underline hover:!text-brian-blue"
