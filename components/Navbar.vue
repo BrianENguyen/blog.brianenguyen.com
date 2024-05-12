676ee5264549941be532072d1f12cd651153e515
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-12 10:10:04 -0500

    bug: navbar not working on mobile

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -45,6 +45,7 @@ function toggleMobileMenu() {
   <nav
     class="fixed sm:hidden bg-white dark:bg-#0e1111 fixed w-[100%] h-100px z-[1] top-0 left-0 transition duration-300 shadow-2xl"
   >
+    {{ isToggled }}
     <div
       class="py-8 bg-white dark:bg-#0e1111 md:flex items-center justify-between mx-auto filter-none"
     >
