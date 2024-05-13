97908c73ae3ebfc53c923e4799d859f0ffb1b18b
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-13 16:35:12 -0500

    feat: BlogListing

---
+++ components/blog/BlogListing.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps(['blog']);
+</script>
+
+<template>
+  <div class="mb-4">
+    <NuxtLink
+      :to="blog._path"
+      class="text-black dark:!text-white no-underline hover:!text-brian-blue"
+    >
+      <time class="block text-gray">
+        <span>
+          {{ formatDate(blog.date) }}
+        </span>
+        <span v-if="blog.edited" class="italic">
+          (Edited on {{ formatDate(blog.edited) }})
+        </span>
+      </time>
+      <span>{{ blog.title }}</span>
+    </NuxtLink>
+  </div>
+</template>
