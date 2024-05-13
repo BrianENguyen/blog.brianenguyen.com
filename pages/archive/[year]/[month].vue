12fa6e6b380b81b208ca58fd09ef1da4477c65f4
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-13 14:53:22 -0500

    feat: month view loading

--- pages/archive/[year]/[month].vue
+++ pages/archive/[year]/[month].vue
@@ -21,6 +21,7 @@ useHead({
 
 
 const blogs = ref([]);
+const fetchStatus = ref('fetching');
 
 async function fetchBlogs() {
   const data = await queryContent('blog')
@@ -37,32 +38,59 @@ async function fetchBlogs() {
     }
   })
   blogs.value = filteredBlogs;
+  fetchStatus.value = 'success';
 }
 
 onMounted(() => {
+  setTimeout(() => {
+    if (fetchStatus.value !== 'success') {
+      fetchStatus.value = 'delayed';
+    }
+  }, 2000);
   fetchBlogs();
 })
 </script>
 
 <template>
-  <section :id="`${yearParam}-${monthParam}`" v-if="blogs.length">
-    <h1>Blogs from {{ yearParam }} {{ convertMonthToName(monthParam) }}</h1>
-    <div v-for="(blog, i) in blogs" :key="i" class="mb-4">
-      <NuxtLink
-        :to="blog._path"
-        class="text-black dark:!text-white no-underline hover:!text-brian-blue"
-      >
-        <time class="block text-gray">{{ formatDate(blog.date) }}</time>
-        <span>{{ blog.title }}</span>
-      </NuxtLink>
-    </div>
+  <div>
+    <section
+      v-if="fetchStatus === 'fetching' || fetchStatus === 'delayed'"
+      :id="`${yearParam}-${monthParam}`"
+    >
+      <h1>Blogs from {{ yearParam }} {{ convertMonthToName(monthParam) }}</h1>
+      <p>Loading...</p>
+      <p v-if="fetchStatus === 'delayed'">
+        This seems to be taking a while to fetch the blogs. Maybe reload the
+        page?
+      </p>
+    </section>
+    <section
+      :id="`${yearParam}-${monthParam}`"
+      v-if="blogs.length && fetchStatus === 'success'"
+    >
+      <h1>Blogs from {{ yearParam }} {{ convertMonthToName(monthParam) }}</h1>
+      <div v-for="(blog, i) in blogs" :key="i" class="mb-4">
+        <NuxtLink
+          :to="blog._path"
+          class="text-black dark:!text-white no-underline hover:!text-brian-blue"
+        >
+          <time class="block text-gray">{{ formatDate(blog.date) }}</time>
+          <span>{{ blog.title }}</span>
+        </NuxtLink>
+      </div>
 
-    <BackButton :year="yearParam" />
-    <BackButton />
-  </section>
+      <BackButton :year="yearParam" />
+      <BackButton />
+    </section>
 
-  <section :id="`${yearParam}-${monthParam}`" v-else>
-    <h1>No blogs from {{ yearParam }} {{ convertMonthToName(monthParam) }}!</h1>
-    <BackButton />
-  </section>
+    <section
+      v-else-if="!blogs.length && fetchStatus === 'success'"
+      :id="`${yearParam}-${monthParam}`"
+    >
+      <h1>
+        No blogs from {{ yearParam }} {{ convertMonthToName(monthParam) }}!
+      </h1>
+      <BackButton />
+    </section>
+  </div>
 </template>
