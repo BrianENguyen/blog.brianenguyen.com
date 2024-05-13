d53b971a99708ac296e3cddeeefe97c9b6cdd942
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-13 16:35:53 -0500

    feat: implement BlogListing

--- pages/archive/[year]/[month].vue
+++ pages/archive/[year]/[month].vue
@@ -4,6 +4,7 @@ import { convertMonthToName } from '~/composables/convertMonthToName';
 import { getYear } from '~/composables/getYear';
 import { getMonth } from '~/composables/getMonth';
 import { convertMonthToDigit } from '~/composables/convertMonthToDigit';
+import BlogCard from '../../../components/blog/BlogListing.vue';
 
 const route = useRoute()
 const yearParam = route.params.year;
@@ -18,7 +19,6 @@ useHead({
     }
   ]
 })
-
 
 const blogs = ref([]);
 const fetchStatus = ref('fetching');
@@ -69,23 +69,7 @@ onMounted(() => {
       v-if="blogs.length && fetchStatus === 'success'"
     >
       <h1>Blogs from {{ yearParam }} {{ convertMonthToName(monthParam) }}</h1>
-      <div v-for="(blog, i) in blogs" :key="i" class="mb-4">
-        <NuxtLink
-          :to="blog._path"
-          class="text-black dark:!text-white no-underline hover:!text-brian-blue"
-        >
-          <time class="block text-gray">
-            <span>
-              {{ formatDate(blog.date) }}
-            </span>
-            <span v-if="blog.edited" class="italic">
-              (Edited on {{ formatDate(blog.edited) }})
-            </span>
-          </time>
-          <span>{{ blog.title }}</span>
-        </NuxtLink>
-      </div>
-
+      <BlogListing :blog="blog" v-for="(blog, i) in blogs" :key="i" />
       <BackButton :year="yearParam" />
       <BackButton />
     </section>
--- pages/archive/[year]/index.vue
+++ pages/archive/[year]/index.vue
@@ -62,22 +62,7 @@ onMounted(() => {
 
     <section v-if="blogs.length && fetchStatus === 'success'" :id="yearParam">
       <h1>Blogs from {{ yearParam }}</h1>
-      <div v-for="(blog, i) in blogs" :key="i" class="mb-4">
-        <NuxtLink
-          :to="blog._path"
-          class="text-black dark:!text-white no-underline hover:!text-brian-blue"
-        >
-          <time class="block text-gray">
-            <span>
-              {{ formatDate(blog.date) }}
-            </span>
-            <span v-if="blog.edited" class="italic">
-              (Edited on {{ formatDate(blog.edited) }})
-            </span>
-          </time>
-          <span>{{ blog.title }}</span>
-        </NuxtLink>
-      </div>
+      <BlogListing :blog="blog" v-for="(blog, i) in blogs" :key="i" />
       <BackButton />
     </section>
 
