f70630b639daa5e2d9c1f777b6a40ffd5feb3a2e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2024-05-13 14:40:49 -0500

    feat: year page fetching status

--- pages/archive/[year]/index.vue
+++ pages/archive/[year]/index.vue
@@ -13,9 +13,11 @@ useHead({
       content: `List of blogs in ${yearParam}`
     }
   ]
-})
+});
+
 
 const blogs = ref([]);
+const fetchStatus = ref('fetching');
 
 async function fetchBlogs() {
   const data = await queryContent('blog')
@@ -31,29 +33,53 @@ async function fetchBlogs() {
     }
   })
   blogs.value = filteredBlogs;
+  fetchStatus.value = 'success';
 }
 
 onMounted(() => {
+  setTimeout(() => {
+    if (fetchStatus.value !== 'success') {
+      fetchStatus.value = 'delayed';
+    }
+  }, 2000)
   fetchBlogs();
 })
 </script>
-<template>
-  <section :id="yearParam" v-if="blogs.length">
-    <h1>Blogs from {{ yearParam }}</h1>
-    <div v-for="(blog, i) in blogs" :key="i" class="mb-4">
-      <NuxtLink
-        :to="blog._path"
-        class="text-black dark:!text-white no-underline hover:!text-brian-blue"
-      >
-        <time class="block text-gray">{{ formatDate(blog.date) }}</time>
-        <span>{{ blog.title }}</span>
-      </NuxtLink>
-    </div>
-    <BackButton />
-  </section>
 
-  <section :id="yearParam" v-else>
-    <h1>No blogs from {{ yearParam }}!</h1>
-    <BackButton />
-  </section>
+<template>
+  <div>
+    <section
+      v-if="fetchStatus === 'fetching' || fetchStatus === 'delayed'"
+      :id="yearParam"
+    >
+      <h1>Blogs from {{ yearParam }}</h1>
+      <p>Loading...</p>
+      <p v-if="fetchStatus === 'delayed'">
+        This seems to be taking a while to fetch the blogs. Maybe reload the
+        page?
+      </p>
+    </section>
+
+    <section v-if="blogs.length && fetchStatus === 'success'" :id="yearParam">
+      <h1>Blogs from {{ yearParam }}</h1>
+      <div v-for="(blog, i) in blogs" :key="i" class="mb-4">
+        <NuxtLink
+          :to="blog._path"
+          class="text-black dark:!text-white no-underline hover:!text-brian-blue"
+        >
+          <time class="block text-gray">{{ formatDate(blog.date) }}</time>
+          <span>{{ blog.title }}</span>
+        </NuxtLink>
+      </div>
+      <BackButton />
+    </section>
+
+    <section
+      v-else-if="!blogs.length && fetchStatus === 'success'"
+      :id="yearParam"
+    >
+      <h1>No blogs from {{ yearParam }}!</h1>
+      <BackButton />
+    </section>
+  </div>
 </template>
